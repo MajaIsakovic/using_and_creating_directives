3bb59c7ba80f6903d23848f20f769b49c1a41a70
Maja Isakovic <majamaja733@hotmail.com>
2019-11-25 17:34:50 +0100

    creating a directive

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// create directive:
+// first param: name of the directive
+// second param: object configuring the directive
+Vue.directive('highlight', {
+    bind(el, binding){
+      // el.style.backgroundColor = 'gray';
+      el.style.backgroundColor = binding.value;
+    }
+
+});
 
 new Vue({
   render: h => h(App),
